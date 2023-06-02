60bdac32c0a2a02cc0017c7852638867b43c3bde
atharva80 <atharva80>
2023-06-02 15:21:06 +0530

    initial

--- App.js
+++ App.js
@@ -1,11 +1,50 @@
 import React, { useState } from 'react';
 import { View, Button, Image, Alert, StyleSheet, Text } from 'react-native';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
 import * as ImagePicker from 'expo-image-picker';
 import * as MediaLibrary from 'expo-media-library';
 
-const App = () => {
-  const [photo, setPhoto] = useState(null);
+//Declares a functional component named SubmitPhotoScreen that takes a navigation prop as input.
+const SubmitPhotoScreen = ({ navigation }) => {
+  const photoUri = navigation.getParam('photoUri', null); //Retrieves the value of the 'photoUri' parameter from the navigation prop.
 
+  const handleCropPhoto = () => {
+    // Photo cropping logic here
+  };
+
+  const handleDeletePhoto = () => {
+    navigation.goBack();
+  };
+
+  const handleSubmitPhoto = () => {
+    // Photo submission logic here
+  };
+
+  //UI and layout elements for the Photo Submit Screen
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Image
+        source={{ uri: photoUri }}
+        style={{ width: 200, height: 200, marginBottom: 10 }}
+      />
+      <View style={styles.buttonContainer}>
+        <View style={styles.buttonGroup}>
+          <Button title="Crop" onPress={handleCropPhoto} /><View style={styles.spacing} />
+          <Button title="Delete" onPress={handleDeletePhoto} />
+        </View>
+        <View style={styles.spacing} />
+        <Button title="Submit" onPress={handleSubmitPhoto} />
+      </View>
+    </View>
+  );
+};
+
+//Declares a functional component named App that takes a navigation prop as input.
+const App = ({ navigation }) => {
+  const [photo, setPhoto] = useState(null); //Uses the useState hook to declare a state variable named photo and a function named setPhoto to update its value. The initial value of photo is null.
+
+  //Declares an asynchronous function named handleCapturePhoto that requests camera permissions using ImagePicker.requestCameraPermissionsAsync
   const handleCapturePhoto = async () => {
     const { status } = await ImagePicker.requestCameraPermissionsAsync();
 
@@ -15,11 +54,12 @@ const App = () => {
       });
 
       if (!result.cancelled) {
-        setPhoto(result.uri);
+        navigation.navigate('SubmitPhoto', { photoUri: result.uri });
       }
     }
   };
 
+  //Declares an asynchronous function named handleBrowsePhoto that requests media library permissions using ImagePicker.requestMediaLibraryPermissionsAsync
   const handleBrowsePhoto = async () => {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
@@ -29,11 +69,12 @@ const App = () => {
       });
 
       if (!result.cancelled) {
-        setPhoto(result.uri);
+        navigation.navigate('SubmitPhoto', { photoUri: result.uri });
       }
     }
   };
 
+  //Declares an asynchronous function named handleSubmitPhoto that checks if a photo is selected (photo state variable) and then calls MediaLibrary.createAssetAsync to create an asset from the photo.
   const handleSubmitPhoto = async () => {
     if (photo) {
       const asset = await MediaLibrary.createAssetAsync(photo);
@@ -43,6 +84,7 @@ const App = () => {
     }
   };
 
+  //UI and layout elements for main app screen
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       {photo && (
@@ -51,19 +93,26 @@ const App = () => {
           style={{ width: 200, height: 200, marginBottom: 10 }}
         />
       )}
-      <Text style={styles.sectionTitle}>Welcome!</Text><View style={styles.spacing} />
+      <Text style={styles.sectionTitle}>Welcome!</Text>
+      <View style={styles.spacing} />
       <View style={styles.buttonContainer}>
-        <Button title="Capture Photo" onPress={handleCapturePhoto} style={styles.button} /><View style={styles.spacing} />
-        <Button title="Browse Photo" onPress={handleBrowsePhoto} style={styles.button} /><View style={styles.spacing} />
-        <Button title="Submit Photo" onPress={handleSubmitPhoto} disabled={!photo} style={styles.button} />
+        <Button title="Capture Photo" onPress={handleCapturePhoto} style={styles.button} />
+        <View style={styles.spacing} />
+        <Button title="Browse Photo" onPress={handleBrowsePhoto} style={styles.button} />
+        <View style={styles.spacing} />
       </View>
     </View>
-    
   );
 };
+
+//Defines a style object using the StyleSheet.create function
 const styles = StyleSheet.create({
   buttonContainer: {
     width: '50%',
+  },
+  buttonGroup: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   button: {
     flex: 1,
@@ -72,9 +121,25 @@ const styles = StyleSheet.create({
   spacing: {
     marginVertical: 15,
   },
-  sectionTitle:{
+  sectionTitle: {
     fontSize: 24,
     fontWeight: 'bold',
   },
 });
-export default App;
+
+//Creates a stack navigator using createStackNavigator and defines two screens: 'Home' and 'SubmitPhoto'
+const AppNavigator = createStackNavigator(
+  {
+    Home: { screen: App },
+    SubmitPhoto: { screen: SubmitPhotoScreen },
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+//Creates a container component using createAppContainer and passes the AppNavigator as the argument.
+const AppContainer = createAppContainer(AppNavigator);
+
+//Exports the AppContainer component as the default export of the module.
+export default AppContainer;
